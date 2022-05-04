f38daaf93f79760ee89cfe15fca9df941d3748a5
FengYungz <lucasfeng83@gmail.com>
2022-05-03 23:23:37 -0300

    :white_check_mark: upload files

---
+++ Labs/Lab2/fatorial.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ 
+int fatorial(int n);
+ 
+/**
+ * Ponto de entrada do programa (entry point).
+ */
+int main(int argc, char** argv) {
+  int n;
+  scanf("%d", &n);
+ 
+  printf("%d\n", fatorial(n));
+ 
+  return 0;
+}
+ 
+/**
+ * Calcula o fatorial do número recebido e retorna o resultado.
+ */
+int fatorial(int n) {
+  // Por enquanto, essa função não faz nada de muito útil... Ela apenas
+  // retorna o número recebido.
+  // Altere esta implementação para retornar o resultado correto.
+  
+  
+  if((n == 1) || (n == 0)){
+      return 1;
+  }
+  else{
+      return fatorial(n-1)*n;
+  }
+ 
+  return n;
+}
